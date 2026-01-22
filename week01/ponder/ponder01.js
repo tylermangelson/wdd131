cf05de8c6e0d05ee71cfe59ec5777d63439d67cd
tylermangelson <realtylermangelson@gmail.com>
2026-01-21 21:18:15 -0700

    Repo fix

---
+++ week01/ponder/ponder01.js
@@ -0,0 +1,17 @@
+const PI = 3.14;
+let radius = 3;
+
+
+const one = 1;
+const two = '2';
+                    
+
+let course = "CSE131"; //global scope
+if (true) {
+    let student = "John";
+    console.log(course);  //works just fine, course is global
+    console.log(student); //works just fine, it's being accessed within the block
+}
+console.log(course); //works fine, course is global
+console.log(student); //does not work, can't access a block variable outside the block
+                    
