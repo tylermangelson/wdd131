f9c840a17a05824c353cc80f631ad91a2fbda125
tylermangelson <realtylermangelson@gmail.com>
2026-01-26 14:26:26 -0700

    finished coolpics

---
+++ week03/coolpics/coolpics.js
@@ -0,0 +1,12 @@
+const nav = document.querySelector('nav');
+const toggleButton = document.querySelector('.menu-btn')
+
+toggleButton.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth >= 700) {
+        nav.classList.remove('active')
+    }
+});
